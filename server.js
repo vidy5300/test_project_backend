b6719d72a227832535134dc7be804d9fb1cec1eb
viji.vidyadharan <viji.vidyadharan@empowermx.com>
2025-06-11 15:00:52 +0530

    Used port dynamically

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
